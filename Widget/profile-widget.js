1d072c04b88355ebe46d3f5a70a20a1a4e012048
AlexeyAnshakov <alexey.anshakov@gmail.com>
2014-12-05 08:04:05 +0530

    Login widget added

---
+++ Widget/profile-widget.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular
+    .module('appProfile', [])
+    .directive('profileWidget', function () {
+        return {
+            restrict: 'A',
+            templateUrl: 'http://wrio.s3-website-us-east-1.amazonaws.com/Widgets/profile-widget.html',
+            scope: {},
+            controller: function ($scope) {
+                $scope.title = 'profile widget directive';
+            }
+        };
+    });
