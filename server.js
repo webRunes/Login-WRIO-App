6c9c0d1216f68d5004f70d4f4db2cc2fcacb8726
bhushano7 <bhushan2250@gmail.com>
2015-04-12 00:01:48 +0530

    twitter login changes

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ var nconf = require("./wrio_nconf.js").init();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(session(
-	{
-		secret: 'keyboard cat',
-		saveUninitialized: true,
-		resave: true,
-		key: 'sid'
-	}
+    {
+        secret: 'keyboard cat',
+        saveUninitialized: true,
+        resave: true,
+        key: 'sid'
+    }
 ));
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -29,98 +29,122 @@ app.use(express.static(__dirname + '/public'));
 
 
 passport.serializeUser(function (user, done) {
-	done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function (obj, done) {
-	done(null, obj);
+    done(null, obj);
 });
 
 passport.use(new FacebookStrategy({
-		clientID: nconf.get('api:facebook:clientId'),
-		clientSecret: nconf.get('api:facebook:clientSecret'),
-		callbackURL: nconf.get('api:facebook:callbackUrl')
-	},
-	function (accessToken, refreshToken, profile, done) {
-		process.nextTick(function () {
-			return done(null, profile);
-		});
-	}
+        clientID: nconf.get('api:facebook:clientId'),
+        clientSecret: nconf.get('api:facebook:clientSecret'),
+        callbackURL: nconf.get('api:facebook:callbackUrl')
+    },
+    function (accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
 ));
 
 passport.use(new TwitterStrategy({
-		consumerKey: nconf.get("api:twitterLogin:consumerKey"),
-		consumerSecret: nconf.get("api:twitterLogin:consumerSecret"),
-		callbackURL: nconf.get("api:twitterLogin:callbackUrl")
-	},
-	function (accessToken, refreshToken, profile, done) {
-		process.nextTick(function () {
-			return done(null, profile);
-		});
-	}
+        consumerKey: nconf.get("api:twitterLogin:consumerKey"),
+        consumerSecret: nconf.get("api:twitterLogin:consumerSecret"),
+        callbackURL: nconf.get("api:twitterLogin:callbackUrl")
+    },
+    function (accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
 ));
 
 passport.use(new GitHubStrategy({
-		clientID: nconf.get('api:gitHub:clientId'),
-		clientSecret: nconf.get('api:gitHub:clientSecret'),
-		callbackURL: nconf.get('api:gitHub:callbackUrl')
-	},
-	function (accessToken, refreshToken, profile, done) {
-		process.nextTick(function () {
-			return done(null, profile);
-		});
-	}
+        clientID: nconf.get('api:gitHub:clientId'),
+        clientSecret: nconf.get('api:gitHub:clientSecret'),
+        callbackURL: nconf.get('api:gitHub:callbackUrl')
+    },
+    function (accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
 ));
 
 app.get('/', function (request, response) {
-	response.render('index', {user: request.user});
+    response.render('index', {user: request.user});
 });
 
 app.get('/loginTwitter', function (request, response) {
-	response.render('login', {user: request.user});
+    response.render('login', {user: request.user});
 });
 
 app.get('/account', ensureAuthenticated, function (request, response) {
-	response.render('account', {user: request.user});
+    response.render('account', {user: request.user});
 });
 
 app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
 app.get('/auth/facebook/callback',
-	passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login'}),
-	function (request, response) {
-		response.redirect('/');
-	});
+    passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login'}),
+    function (request, response) {
+        response.redirect('/');
+    });
 
-app.get('/auth/twitter', passport.authenticate('twitter', {scope: 'email'}));
+app.get('/auth/twitter', function (request, response, next) {
+    request.session.redirect = request.headers['referer'];
+    next();
+}, passport.authenticate('twitter', {scope: 'email'}));
 
-app.get('/auth/twitter/callback',
-	passport.authenticate('twitter', {successRedirect: '/', failureRedirect: '/login'}),
-	function (request, response) {
-		response.redirect('/');
-	});
+app.get('/auth/twitter/callback', function (request, response, next) {
+    passport.authenticate('twitter', function (error, user, info) {
+        // This is the default destination upon successful login.
+        var redirectUrl = '/';
 
-app.get('/logout', function(req, res){
-	req.logout();
-	res.redirect('/');
+        if (error) {
+            return next(error);
+        }
+        if (!user) {
+            return response.redirect('/');
+        }
+
+        // If we have previously stored a redirectUrl, use that,
+        // otherwise, use the default.
+        if (request.session.redirectUrl) {
+            redirectUrl = request.session.redirectUrl;
+            request.session.redirectUrl = null;
+        }
+        request.logIn(user, function (error) {
+            if (error) {
+                return next(error);
+            }
+        });
+        response.redirect(redirectUrl);
+    })(request, response, next);
+});
+
+app.get('/logout', function (request, res) {
+    request.logout();
+    response.redirect('/');
 });
 
 app.get('/auth/github', passport.authenticate('github'));
 app.get('/auth/git-hub/callback',
-	passport.authenticate('github', {failureRedirect: '/login'}),
-	function (req, res) {
-		res.redirect('/');
-	});
+    passport.authenticate('github', {failureRedirect: '/login'}),
+    function (request, res) {
+        response.redirect('/');
+    });
 
 app.get('/logout', function (request, response) {
-	request.logout();
-	response.redirect('/');
+    request.logout();
+    response.redirect('/');
 });
 
 function ensureAuthenticated(request, response, next) {
-	if (request.isAuthenticated()) {
-		return next();
-	}
-	response.redirect('/login')
+    if (request.isAuthenticated()) {
+        return next();
+    }
+    response.redirect('/login')
 }
-console.log('Hello Travis!');
+//console.log('Hello Travis!');
